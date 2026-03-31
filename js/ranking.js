ea194736666fdd75339b2c151b23f04e94884e34
HyeRyeong125 <aksux0199@gmail.com>
2026-03-31 13:59:20 +0900

    Initial commit: 퀴즈 앱 프로젝트

---
+++ js/ranking.js
@@ -0,0 +1,16 @@
+/**
+ * 상식 퀴즈 챌린지 - 순위 시스템 (3단계에서 구현)
+ */
+const Ranking = (() => {
+  function save(data) {
+    // 3단계에서 구현
+  }
+
+  function render() {
+    // 3단계에서 구현
+    const container = document.getElementById('ranking-table-container');
+    container.innerHTML = '<p class="empty-ranking">아직 기록이 없습니다.</p>';
+  }
+
+  return { save, render };
+})();
